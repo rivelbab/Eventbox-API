5879ce867ba6651cc6cd69b47eaddbd2c2108c9c
Rivel Babindamana <rivelbab@gmail.com>
2019-10-27 02:47:36 +0100

    project initialisation

---
+++ src/main/java/com/eeventbox/EeventboxApplication.java
@@ -0,0 +1,13 @@
+package com.eeventbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeventboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeventboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eeventbox/EeventboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeventbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeventboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
